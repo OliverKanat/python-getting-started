2614a40e43acdca95a98b547f4c7d8e34ea1efb0
OliverKanat <kanat.oliver@gmail.com>
2020-05-21 20:37:30 +0200

    fix release

--- release.sh
+++ release.sh
@@ -1,1 +1,2 @@
-wget -O ./staticfiles/index.html  --post-data="rss_feed=https://www.sme.sk/rss-title&sort=asc" https://rsskanat.herokuapp.com/
+wget -O ./index.html  --post-data="rss_feed=https://www.sme.sk/rss-title&sort=asc" https://rsskanat.herokuapp.com/
+curl -T ./index.html  -u webpubfiit:Dmg6zi5F@8@BCA7 ftp://files.000webhost.com/public_html/index.html
